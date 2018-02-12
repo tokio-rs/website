3b5f28cb0b7de26e07ae6b5be2cd0f7b63e23a4b
Carl Lerche <me@carllerche.com>
2018-02-12 13:35:23 -0800

    Add deploy script to repo

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+set -e -x
+
+# Sync the website
+aws s3 sync public s3://tokio.rs --acl public-read --delete
+
+# Create a cloudfront invalidation
+echo -n '{"Paths": {"Items": ["/*"], "Quantity": 1}, "CallerReference":"' >> payload.json &&
+echo -n "travis-$(date)" >> payload.json &&
+echo -n '"}' >> payload.json
+
+# Submit the invalidation
+~/.local/bin/aws cloudfront create-invalidation --invalidation-batch file://payload.json --distribution-id ELMMDTQCB9BNA
