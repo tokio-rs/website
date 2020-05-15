aa712fc42abe475b1ea2171411b9d32cc09fb41e
Carl Lerche <me@carllerche.com>
2020-05-15 13:42:47 -0700

    stub hero and logo sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,33 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
     <Layout>
-      <p className="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              The asynchronous run-time for the Rust programming language
+            </h1>
+            <h2 className="subtitle">
+              Tokio is an open source library providing an asynchronous, event driven platform for
+              building fast, reliable, and lightweight network applications. It leverages Rust's
+              ownership and concurrency model to ensure thread safety.
+            </h2>
+            <a class="button">Get Started</a>
+          </div>
+        </div>
+      </section>
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              Built by the community, for the community.
+            </h1>
+            <h2 className="subtitle">
+              TODO: logos
+            </h2>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
