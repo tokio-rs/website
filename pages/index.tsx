ce595d43625df832390c671033e386771e4c80fe
Carl Lerche <me@carllerche.com>
2020-05-15 20:37:30 -0700

    stub major libs

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,6 +37,81 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section>
+        <div className="columns">
+          <div className="column">
+            <h1>Fast. Reliable. Lightweight.</h1>
+            <h2>Building on top of Rust, Tokio provides blazingly fast performance, making it an ideal choice for high performance server applications.</h2>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image">
+                      <img src="/img/icons/tokio.svg" alt="Placeholder image"/>
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">Tokio Runtime</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image">
+                      <img src="/img/icons/hyper.svg" alt="Placeholder image"/>
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">Hyper</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image">
+                      <img src="/img/icons/tonic.svg" alt="Placeholder image"/>
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-4">Tonic</p>
+                  </div>
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </Layout>
   )
 }
