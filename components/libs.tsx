9e0e22b5758204488d67d52a51f5c02bfb57bcca
Carl Lerche <me@carllerche.com>
2020-05-15 22:07:53 -0700

    lib card layout

--- components/libs.tsx
+++ components/libs.tsx
@@ -1,3 +1,5 @@
+import classnames from "classnames";
+
 export default function Libs() {
     const items = [
         {
@@ -16,7 +18,7 @@ export default function Libs() {
           desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
         },
     ].map(({id, name, desc}) => (
-      <div className="column is-half">
+      <div key={id} className={classnames("column", "is-half", "tk-lib", `tk-lib-${id}`)}>
         <div className="card">
           <div className="card-content">
             <div className="media">
@@ -41,8 +43,10 @@ export default function Libs() {
         <div className="container">
           <div className="columns is-multiline">
             <div className="column is-half">
-              <h1>Fast. Reliable. Lightweight.</h1>
-              <h2>Building on top of Rust, Tokio provides blazingly fast performance, making it an ideal choice for high performance server applications.</h2>
+              <div className="hero is-medium">
+                <h1 className="title">Fast. Reliable. Lightweight.</h1>
+                <h2 className="subtitle">Building on top of Rust, Tokio provides blazingly fast performance, making it an ideal choice for high performance server applications.</h2>
+              </div>
             </div>
             {items}
           </div>
