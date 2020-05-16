9b2ded56d3d103a7684d4ab50eec741703335cba
Carl Lerche <me@carllerche.com>
2020-05-15 21:29:29 -0700

    tile libs section

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,79 +20,80 @@ export default function Home() {
         </div>
       </section>
       <Logos/>
-      <section>
-        <div className="columns">
-          <div className="column">
-            <h1>Fast. Reliable. Lightweight.</h1>
-            <h2>Building on top of Rust, Tokio provides blazingly fast performance, making it an ideal choice for high performance server applications.</h2>
-          </div>
+      <section className="tk-libs">
+        <div className="container">
+          <div className="columns is-multiline">
+            <div className="column is-half">
+              <h1>Fast. Reliable. Lightweight.</h1>
+              <h2>Building on top of Rust, Tokio provides blazingly fast performance, making it an ideal choice for high performance server applications.</h2>
+            </div>
 
-          <div className="column">
-            <div className="card">
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-left">
-                    <figure className="image">
-                      <img src="/img/icons/tokio.svg" alt="Placeholder image"/>
-                    </figure>
+            <div className="column is-half">
+              <div className="card">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image">
+                        <img src="/img/icons/tokio.svg" alt="Placeholder image"/>
+                      </figure>
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-4">Tokio Runtime</p>
+                    </div>
                   </div>
-                  <div className="media-content">
-                    <p className="title is-4">Tokio Runtime</p>
+                  <div className="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
                   </div>
                 </div>
-                <div className="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                  <a href="#">#css</a> <a href="#">#responsive</a>
+              </div>
+            </div>
+
+            <div className="column is-half">
+              <div className="card">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image">
+                        <img src="/img/icons/hyper.svg" alt="Placeholder image"/>
+                      </figure>
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-4">Hyper</p>
+                    </div>
+                  </div>
+                  <div className="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="column is-half">
+              <div className="card">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image">
+                        <img src="/img/icons/tonic.svg" alt="Placeholder image"/>
+                      </figure>
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-4">Tonic</p>
+                    </div>
+                  </div>
+                  <div className="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <div className="column">
-            <div className="card">
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-left">
-                    <figure className="image">
-                      <img src="/img/icons/hyper.svg" alt="Placeholder image"/>
-                    </figure>
-                  </div>
-                  <div className="media-content">
-                    <p className="title is-4">Hyper</p>
-                  </div>
-                </div>
-                <div className="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                  <a href="#">#css</a> <a href="#">#responsive</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="column">
-            <div className="card">
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-left">
-                    <figure className="image">
-                      <img src="/img/icons/tonic.svg" alt="Placeholder image"/>
-                    </figure>
-                  </div>
-                  <div className="media-content">
-                    <p className="title is-4">Tonic</p>
-                  </div>
-                </div>
-                <div className="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                  <a href="#">#css</a> <a href="#">#responsive</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
         </div>
       </section>
     </Layout>
