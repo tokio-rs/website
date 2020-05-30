fbd2703026d472855bf651c801aa841342ff2dfe
Carl Lerche <me@carllerche.com>
2020-05-29 17:02:17 -0700

    Lib blurbs (#28)

--- components/stack.tsx
+++ components/stack.tsx
@@ -6,61 +6,74 @@ type StackLayer = {
   short?: string;
   name: string;
   desc: string;
-  zIndex: number,
+  zIndex: number;
+  href?: string;
 };
-
-const PLACEHOLDER =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 const STACK_LAYERS: StackLayer[] = [
   {
     id: "tokio",
     short: "Stack",
     name: "The stack",
-    desc: PLACEHOLDER,
+    desc:
+      "Applications aren't built in a vacuum. The Tokio stack includes everything needed to ship to production, fast.",
     zIndex: 0,
   },
   {
     id: "runtime",
     name: "Runtime",
-    desc: PLACEHOLDER,
+    desc:
+      "Including I/O, timer, filesystem, synchronization, and scheduling facilities, the Tokio runtime is the foundation of asynchronous applications.",
     zIndex: 3,
+    href: "/tokio",
   },
   {
     id: "hyper",
     name: "Hyper",
-    desc: PLACEHOLDER,
+    desc:
+      "An HTTP client and server library supporting both the HTTP 1 and 2 protocols.",
     zIndex: 4,
+    href: "https://github.com/hyperium/hyper",
   },
   {
     id: "tonic",
     name: "Tonic",
-    desc: PLACEHOLDER,
+    desc:
+      "A boilerplate-free gRPC client and server library. The easiest way to expose and consume an API over the network.",
     zIndex: 6,
+    href: "https://github.com/hyperium/tonic",
   },
   {
     id: "tower",
     name: "Tower",
-    desc: PLACEHOLDER,
+    desc:
+      "Modular components for building reliable clients and servers. Includes retry, load-balancing, filtering, request-limiting facilities, and more.",
     zIndex: 5,
+    href: "https://github.com/tower-rs/tower",
   },
   {
     id: "mio",
     name: "Mio",
-    desc: PLACEHOLDER,
+    desc:
+      "Minimal portable API on top of the operataing-system's evented I/O API.",
     zIndex: 2,
+    href: "https://github.com/tokio-rs/mio",
   },
   {
     id: "tracing",
     name: "Tracing",
-    desc: PLACEHOLDER,
+    desc:
+      "Unified visibility into the application and libraries. Provides structured, event-based, data collection and logging.",
     zIndex: 0,
+    href: "https://github.com/tokio-rs/tracing",
   },
   {
     id: "bytes",
     name: "Bytes",
-    desc: PLACEHOLDER,
+    desc:
+      "At the core, networking applications manipulate byte streams. Bytes provides a rich set of utilities for manipulating byte arrays.",
     zIndex: 1,
+    href: "https://github.com/tokio-rs/bytes",
   },
 ];
 
@@ -105,9 +118,11 @@ const Layer: FC<{ layer: StackLayer }> = ({ layer }) => (
       </div>
       <div className="content">
         <h2 className="subtitle">{layer.desc}</h2>
-        <p className="learn-more has-text-right">
-          <a href="#">Learn more ➔</a>
-        </p>
+        {layer.href && (
+          <p className="learn-more has-text-right">
+            <a href={layer.href}>Learn more ➔</a>
+          </p>
+        )}
       </div>
     </div>
   </div>
@@ -128,13 +143,13 @@ export default function Stack() {
 
           <div className="column is-half">
             <div className="container anchor">
-              {STACK_LAYERS.slice(1).map(({id, zIndex}) => (
+              {STACK_LAYERS.slice(1).map(({ id, zIndex }) => (
                 <img
                   key={id}
                   className="tk-stack-active"
                   data-stack-id={id}
                   src={"/img/stack-" + id + ".svg"}
-                  style={{zIndex: zIndex}}
+                  style={{ zIndex: zIndex }}
                 />
               ))}
               {/* Special handling */}
@@ -142,7 +157,7 @@ export default function Stack() {
                 id="tk-stack-lines"
                 data-stack-id="lines"
                 src="/img/stack-lines.svg"
-                style={{zIndex: 7}}
+                style={{ zIndex: 7 }}
               />
             </div>
           </div>
