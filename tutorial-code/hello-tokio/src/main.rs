64d774e38a511709a47c12d8147a67c5967327ca
Carl Lerche <me@carllerche.com>
2020-06-12 14:31:57 -0700

    Include tutorial-code in a crate (#45)

---
+++ tutorial-code/hello-tokio/src/main.rs
@@ -0,0 +1,17 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    // Open a connection to the mini-redis address.
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // Get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
+}
