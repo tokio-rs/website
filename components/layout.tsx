3f794881eb8805be0de7c5ddf28f5ebe1a092e9f
skim <skim@swainslane.co>
2020-07-29 15:46:40 -0700

    fixes favicon path

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,8 +13,8 @@ const Layout: FC<Props> = ({ blog, children }) => (
     <Head>
       <title>Tokio</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="alternate icon" type="image/png" href="favicon-32x32.png" />
-      <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+      <link rel="alternate icon" type="image/png" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="alternate" type="application/rss+xml" href="/_next/static/feed.xml" />
 
       <link
