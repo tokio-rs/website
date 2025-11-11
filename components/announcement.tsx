02c998118092dec003c60026a9aef21bd751b01d
Carl Lerche <me@carllerche.com>
2025-11-11 15:11:19 -0800

    Add a TokioConf CFP announcement at the top of the website

---
+++ components/announcement.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const Announcement: FC = () => (
+  <div className="tk-announcement">
+    <div className="tk-announcement-content">
+      <a href="/blog/2025-09-26-announcing-tokio-conf-cfp">
+        The TokioConf 2026 Call For Talk Proposals is now open
+      </a>
+    </div>
+  </div>
+);
+
+export default Announcement;
