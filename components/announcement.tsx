58c9d34d41897dcbe6a45386cc7eb5eb5da41f20
Carl Lerche <me@carllerche.com>
2026-01-13 10:26:21 -0800

    add TokioConf banner (#844)

---
+++ components/announcement.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const Announcement: FC = () => (
+  <div className="tk-announcement">
+    <div className="tk-announcement-content">
+      <a href="https://tokioconf.com">
+        TokioConf 2026 program and tickets are now available! →
+      </a>
+    </div>
+  </div>
+);
+
+export default Announcement;
