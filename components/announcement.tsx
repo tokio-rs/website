203cb9d3123b3df5f80d0e2496fe8cfeb9f1dca4
Carl Lerche <me@carllerche.com>
2026-02-27 17:31:39 -0800

    Add UTM tracking parameters to TokioConf banner link (#847)

--- components/announcement.tsx
+++ components/announcement.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 const Announcement: FC = () => (
   <div className="tk-announcement">
     <div className="tk-announcement-content">
-      <a href="https://tokioconf.com">
+      <a href="https://tokioconf.com/?utm_source=tokio&utm_medium=referral&utm_campaign=discovery">
         TokioConf 2026 program and tickets are now available! →
       </a>
     </div>
